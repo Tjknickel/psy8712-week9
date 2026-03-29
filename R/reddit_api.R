07a366b5f71ce3457e52f702379575434398d1e2
Tjknickel <knick071@umn.edu>
2026-03-29 14:49:02 -0500

    Finished script setting, data importing and cleaning, and visualization sections

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,9 +1,26 @@
 # Script Settings and Resources 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(RedditExtractoR)
 
 # Data Import and Cleaning 
+rstats_df <- find_thread_urls(subreddit = "rstats", period = "month")$url %>%
+  get_thread_content()
+rstats_tbl <-  rstats_df$threads %>%
+  select(post = title,
+         upvotes = upvotes,
+         comments = comments) %>%
+  as.tibble()
 
 # Visualization 
+(rstats_tbl %>%
+    ggplot(aes(upvotes, comments)) +
+  geom_point() +
+  labs(title = "Number of comments vs. upvotes on r/rstats", x = "Number of upvotes", y = "Number of comments")
+) %>%
+  ggsave("../figs/fig1.png",., height = 1080, width = 1920, units = "px", dpi = 600) 
 
 # Analysis 
+cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
 
 # Publication
